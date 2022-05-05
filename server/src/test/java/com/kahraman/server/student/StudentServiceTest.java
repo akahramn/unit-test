39a69653c6920fffd21bac379ad47bc526a28712
Abdullah Kahraman <63452352+akahramn@users.noreply.github.com>
2022-05-06 01:06:14 +0300

    canAddStudent and willThrownMessageWhenEmailIsTaken test methods add.

--- server/src/test/java/com/kahraman/server/student/StudentServiceTest.java
+++ server/src/test/java/com/kahraman/server/student/StudentServiceTest.java
@@ -1,12 +1,19 @@
 package com.kahraman.server.student;
 
+import com.kahraman.server.exception.BadRequestException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 
@@ -30,8 +37,45 @@ class StudentServiceTest {
     }
 
     @Test
-    @Disabled
-    void addStudent() {
+    void canAddStudent() {
+        //Given
+            Student student = new Student(
+                    "Ceren",
+                    "ceren@gmail.com",
+                    Gender.FEMALE
+            );
+        //When
+            underTestStudentService.addStudent(student);
+        //Then
+        ArgumentCaptor<Student> argumentCaptor =
+                ArgumentCaptor.forClass(Student.class);
+
+        verify(studentRepository).save(argumentCaptor.capture());
+
+        Student capturedStudent = argumentCaptor.getValue();
+
+        assertThat(capturedStudent).isEqualTo(student);
+
+    }
+
+    @Test
+    void willThrownMessageWhenEmailIsTaken() {
+        //Given
+        Student student = new Student(
+                "Ceren",
+                "ceren@gmail.com",
+                Gender.FEMALE
+        );
+        given(studentRepository.existsByEmail(student.getEmail())).willReturn(true);
+
+        //When
+        //Then
+
+        assertThatThrownBy(() -> underTestStudentService.addStudent(student)).
+                isInstanceOf(BadRequestException.class).
+                hasMessageContaining("Email " + student.getEmail() + " taken");
+
+        verify(studentRepository, never()).save(any());
     }
 
     @Test
